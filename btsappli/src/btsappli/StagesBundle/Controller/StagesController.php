6ce68b7b0b59acd61e2432adcd65ccc526f2dfd6
Chablo <scorpiiix@gmail.com>
2016-02-01 13:15:51 +0000

    Les changmenents effectués

--- btsappli/src/btsappli/StagesBundle/Controller/StagesController.php
+++ btsappli/src/btsappli/StagesBundle/Controller/StagesController.php
@@ -5,34 +5,16 @@ namespace btsappli\StagesBundle\Controller;
 use StagesBundle\Entity\Task;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use btsappli\StagesBundle\Form\Type;
 
-class StagesController extends Controller
+/*class StagesController extends Controller
 {
-    public function entrepriseFormulaireAction(Request $request)
+    public function newAction()
     {
-        $entreprise = new Entreprise();
+    $task = ...;
+    $form = $this->createForm(new TaskType(), $task);
 
-        $form = $this->createFormBuilder($entreprise)
-            ->add('nom', 'text')
-            ->add('adresse', 'text')
-            ->add('codePostal', 'number')
-            ->add('ville', 'text')
-            ->add('pays', 'text')
-            ->add('adresseMail', 'email')
-            ->add('telephone', 'number')
-            ->add('fax', 'number')
-            ->add('description', 'text')
-            ->add('save', 'submit', array('label' => 'Create Task'))
-            ->getForm();
-        
-        if ($form->isSubmitted() && $form->isValid()) {
-        // ... perform some action, such as saving the task to the database
-
-        return $this->redirectToRoute('task_success');
-        }
-
-        return $this->render('btsappliStagesBundle:Stages:entrepriseFormulaire.html.twig', array(
-            'form' => $form->createView(),
-        ));
+    // ...
     }
 }
+*/
